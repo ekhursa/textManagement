1a637375ecf0baad29fbbafc879cb04e8420af7b
khurram Saleem <khurramsaleem_619@hotmail.com>
2016-08-15 00:55:17 -0400

    Connected REST Routes with front end, now if the user submits a text from the UI, a REST call is made and the text gets persisted in the Database

--- textManagementProject/public/javascripts/angularApp.js
+++ textManagementProject/public/javascripts/angularApp.js
@@ -1,7 +1,27 @@
 var app = angular.module('textManagement', []);
 
 app.controller('textManagementCtrl', [
-'$scope',
-function($scope){
-  $scope.test = 'Please enter a Text';
-}]);
+		'$scope', '$http',
+
+		function ($scope, $http) {
+
+			$scope.test = 'Please enter a Text';
+			$scope.formModel = {};
+			$scope.cityInfo = {};
+			$scope.submitText = function () {
+				$http.post('/texts', $scope.formModel).success(function (data) {
+					$scope.text = data.text;
+				})
+
+				/*$http.get('api.openweathermap.org/data/2.5/weather?q=' + $scope.formModel.city + '&APPID=126e45d0ac2ab9bff45c530cbf7836d7').then(function (response) {
+					$scope.text = response.data;
+				})*/
+
+			}; // add on delete as well
+			/*$http.get('/texts').then(function (response) {
+			$scope.text = response.data[0].text;
+
+			})*/
+		}
+
+	])
