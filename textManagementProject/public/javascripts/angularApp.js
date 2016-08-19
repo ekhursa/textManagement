f670c481884e4641cc7366610ec59d2cd679d1f3
khurram Saleem <khurramsaleem_619@hotmail.com>
2016-08-18 20:50:02 -0400

    Added formModel to the parentScope in order to support Reply to a Text Feature, also added a new GET router for Forum View

--- textManagementProject/public/javascripts/angularApp.js
+++ textManagementProject/public/javascripts/angularApp.js
@@ -1,51 +1,38 @@
 var app = angular.module('textManagement', []);
 
 app.controller('textManagementCtrl', [
-		'$scope', '$http',
+		'$scope', '$http','$rootScope',
 
-		function ($scope, $http) {
-			$scope.doReply = function () {
-				console.log("hey you cilcked me");
-			}
-			$scope.formModel = {};
+		function ($scope, $http, $rootScope) {
+			$rootScope.formModel = {};
 			$scope.cityInfo = {};
-			$scope.selectedText = null;
-
+			
 			$scope.getAllTextByUser = function () {
-				$scope.textByUser = [];
+				$scope.text = new Array();
 				$http.get('/texts').then(function (response) {
 					for (var i = 0; i < response.data.length; i++) {
-						if (response.data[i].user == $scope.formModel.user)
-							$scope.textByUser.push(response.data[i].text);
+						if (response.data[i].user == $rootScope.formModel.user){
+							$scope.text.push(response.data[i].text);
+							console.log(response.data[i].text);
+						}
 					}
 				})
 			}
 
 			$scope.getAllTextsFormatted = function () {
-				$http.get('/texts').then(function (response) {
-					var allTextsArray = response.data;
-					var textArraySorted = [];
-					/*for (var i = 0; i < allTextsArray.length; i++) {
-						if (allTextsArray[i].parentId == null)
-						textArraySorted.push(allTextsArray[i]);
-						for (var j = 0; j < allTextsArray.length; j++) {
-							if (allTextsArray[j].parentId == allTextsArray[i]._id)
-								textArraySorted.push(allTextsArray[j]);
-						}
-					}*/
-					$scope.allTexts = allTextsArray;
-					
+				$http.get('/texts/forumView').then(function (response) {
+					$scope.allTexts = response.data;
 				})
 			}
 
 			$scope.submitText = function () {
-				$http.post('/texts', $scope.formModel).success(function (data) {
+				$http.post('/texts', $rootScope.formModel).success(function (data) {
 					$scope.text = data.text;
-					$scope.formModel.text = null;
-					$scope.formModel.user = null;
+					$rootScope.formModel.text = null;
+					$rootScope.formModel.user = null;
 				})
 
-				$http.get('http://api.openweathermap.org/data/2.5/weather?q=' + $scope.formModel.city + '&units=metric&APPID=126e45d0ac2ab9bff45c530cbf7836d7').then(function (response) {
+				$http.get('http://api.openweathermap.org/data/2.5/weather?q=' + $rootScope.formModel.city + '&units=metric&APPID=126e45d0ac2ab9bff45c530cbf7836d7').then(function (response) {
 					$scope.cityInfo.longitude = response.data.coord.lon;
 					$scope.cityInfo.latitude = response.data.coord.lat;
 					$scope.cityInfo.temperature = response.data.main.temp;
--- textManagementProject/routes/index.js
+++ textManagementProject/routes/index.js
@@ -13,35 +13,44 @@ module.exports = router;
 var mongoose = require("mongoose");
 var Text = mongoose.model("Text");
 
-router.get("/texts", function (req, res, next) {
+router.get("/texts/forumView", function (req, res, next) {
 	Text.find(function (err, texts) {
 		if (err) {
 			return next(err);
 		}
-		//var textsJson = res.json(texts);/*
 		var sortedArray = [];
 		for (var i = 0; i < texts.length; i++) {
-			if (texts[i].parentId == null){
+			if (texts[i].parentId == null) {
 				populateChildren(texts[i], texts);
 				console.log(texts[i].children);
 				sortedArray.push(texts[i]);
 			}
 		}
-		
+
 		res.json(sortedArray);
 
 	});
 });
 
-var populateChildren = function(text, texts) {
+router.get("/texts", function (req, res, next) {
+	Text.find(function (err, texts) {
+		if (err) {
+			return next(err);
+		}
+		res.json(texts);
+
+	});
+});
+
+var populateChildren = function (text, texts) {
 	//text.children = new Array();
 	for (var j = 0; j < texts.length; j++) { ;
-		if (text._id == texts[j].parentId){
+		if (text._id == texts[j].parentId) {
 			text.children.push(texts[j]);
 		}
 	}
 	//console.log(text);
-    
+
 	for (var x = 0; x < text.children.length; x++)
 		populateChildren(text.children[x], texts);
 };
