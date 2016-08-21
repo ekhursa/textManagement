1a4a5de70461288bf664f427a602c372c389c7c0
khurram Saleem <khurramsaleem_619@hotmail.com>
2016-08-20 21:13:39 -0400

    Improved the functionality to reply to texts, now the selected text appears in a red color and user cana lso unselect it

--- textManagementProject/public/javascripts/angularApp.js
+++ textManagementProject/public/javascripts/angularApp.js
@@ -1,17 +1,36 @@
 var app = angular.module('textManagement', []);
 
 app.controller('textManagementCtrl', [
-		'$scope', '$http','$rootScope',
+		'$scope', '$http', '$rootScope',
 
 		function ($scope, $http, $rootScope) {
 			$rootScope.formModel = {};
 			$scope.cityInfo = {};
-			
+			$rootScope.set_color = function (textId) {
+				if (textId == $rootScope.formModel.parentId) {
+					return {
+						color : "red"
+					}
+				} else {
+					return {
+						color : "black"
+					}
+				}
+			}
+
+			$rootScope.set_textId = function (textId) {
+				if (textId == $rootScope.formModel.parentId) {
+					$rootScope.formModel.parentId = null;
+				} else {
+                    $rootScope.formModel.parentId = textId;
+				}
+			}
+
 			$scope.getAllTextByUser = function () {
 				$scope.text = new Array();
 				$http.get('/texts').then(function (response) {
 					for (var i = 0; i < response.data.length; i++) {
-						if (response.data[i].user == $rootScope.formModel.user){
+						if (response.data[i].user == $rootScope.formModel.user) {
 							$scope.text.push(response.data[i].text);
 							console.log(response.data[i].text);
 						}
@@ -32,7 +51,8 @@ app.controller('textManagementCtrl', [
 					$rootScope.formModel.user = null;
 				})
 
-				$http.get('http://api.openweathermap.org/data/2.5/weather?q=' + $rootScope.formModel.city + '&units=metric&APPID=126e45d0ac2ab9bff45c530cbf7836d7').then(function (response) {
+				$http.get('http://api.openweathermap.org/data/2.5/weather?q=' + $rootScope.formModel.city +
+					'&units=metric&APPID=126e45d0ac2ab9bff45c530cbf7836d7').then(function (response) {
 					$scope.cityInfo.longitude = response.data.coord.lon;
 					$scope.cityInfo.latitude = response.data.coord.lat;
 					$scope.cityInfo.temperature = response.data.main.temp;
